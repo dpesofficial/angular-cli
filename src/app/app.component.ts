2ac94ae26e5a3cb113aaef8c426000126c07ac30
Dipesh Thapa <dpesofficial@gmail.com>
2018-11-20 14:09:43 +0545

    http request filter by input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-app-folder';
+  inputval:number;
+
+  constructor(private httpClient:HttpClient){}
+  onNameKeyUp(event:any){
+    this.inputval = event.target.value;
+  }
+  getProfile() {
+    console.log(this.inputval);
+    this.httpClient.get("https://jsonplaceholder.typicode.com/todos/?id=" + this.inputval)
+      .subscribe(
+        (data: any[]) => {
+          console.log(data);
+        }
+      )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NamesComponent } from './names/names.component';
 import { NameTagComponent } from './name-tag/name-tag.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NameTagComponent } from './name-tag/name-tag.component';
     NameTagComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
